0d66768f7e54eeec3337237de053fb1b42cf94ab
Guilherme Dias <guidias200220@gmail.com>
2024-11-06 12:26:17 -0300

    feat: provider added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,32 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/candidate/LoginPageCandidate'
+import RegisterPage from './pages/candidate/RegisterPageCandidate'
 import HomePage from './pages/HomePage'
+import DashboardCandidate from './pages/candidate/DashboardCandidate'
+import ProtectedRoute from './context/ProtectedRoute'
+import { AuthProvider } from './context/AuthProvider'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/register' element={<RegisterPage/>}/>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route
+            path='/dashboard-candidate'
+            element={
+              <ProtectedRoute>
+                <DashboardCandidate />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
