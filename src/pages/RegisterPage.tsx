390f306d2cbf543f3ed5d8b5a922a61a6f9cb228
Guilherme Dias <guidias200220@gmail.com>
2024-11-06 09:57:28 -0300

    feat: RegisterPage created

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,12 @@
+import CandidateRegistrationForm from "../components/register/RegisterForm";
+
+const RegisterPage = () => {
+
+    return (
+        <>
+            <CandidateRegistrationForm/>
+        </>
+    )
+}
+
+export default RegisterPage;
