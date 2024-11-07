f3559338a7eb4d633bb17b96ae32a3419f16206b
Guilherme Dias <guidias200220@gmail.com>
2024-11-07 09:54:46 -0300

    feat: vacancyData created

---
+++ src/types/VacancyData.ts
@@ -0,0 +1,12 @@
+export interface Vacancy {
+    id: number;
+    title: string;
+    name_enterprise: string;
+    level: string;
+    remuneration: string;
+    modality: string;
+    description: string;
+    enterpriseId: number;
+    createdAt: string;
+}
+  
