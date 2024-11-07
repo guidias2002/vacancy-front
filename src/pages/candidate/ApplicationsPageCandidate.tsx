d13b53d242a8d470e4a1061e7990a965ccfea723
Guilherme Dias <guidias200220@gmail.com>
2024-11-07 09:53:15 -0300

    feat: applicationsPageCandidate created

---
+++ src/pages/candidate/ApplicationsPageCandidate.tsx
@@ -0,0 +1,10 @@
+import SidebarCandidate from "../../components/SidebarCandidate"
+
+const ApplicationsPageCandidate = () => {
+
+    return (
+        <SidebarCandidate/>
+    )
+}
+
+export default ApplicationsPageCandidate;
