fff996898089b609735e16d7afd6e2d68ab38339
Guilherme Dias <guidias200220@gmail.com>
2024-11-07 09:51:31 -0300

    feat: authContext added

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import LoginPage from './pages/candidate/LoginPageCandidate'
 import RegisterPage from './pages/candidate/RegisterPageCandidate'
 import HomePage from './pages/HomePage'
 import DashboardCandidate from './pages/candidate/DashboardCandidate'
 import ProtectedRoute from './context/ProtectedRoute'
-import { AuthProvider } from './context/AuthProvider'
+import ProfilePageCandidate from './pages/candidate/ProfilePageCandidate'
+import VacancyPageCandidate from './pages/candidate/VacancyPageCandidate'
+import ApplicationsPageCandidate from './pages/candidate/ApplicationsPageCandidate'
 
 function App() {
+  const routes = [
+    { path: '/', component: <HomePage /> },
+    { path: '/login', component: <LoginPage /> },
+    { path: '/register', component: <RegisterPage /> },
+    { path: '/dashboard-candidate', component: <DashboardCandidate />, protected: true },
+    { path: '/profile-candidate', component: <ProfilePageCandidate />, protected: true },
+    { path: '/vacancy', component: <VacancyPageCandidate />, protected: true },
+    { path: '/my-applications', component: <ApplicationsPageCandidate />, protected: true },
+  ];
 
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route
-            path='/dashboard-candidate'
-            element={
-              <ProtectedRoute>
-                <DashboardCandidate />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
-  )
+    <Routes>
+      {routes.map(({ path, component, protected: isProtected }) => (
+        <Route
+          key={path}
+          path={path}
+          element={
+            isProtected ? <ProtectedRoute>{component}</ProtectedRoute> : component
+          }
+        />
+      ))}
+    </Routes>
+  );
 }
 
-export default App
+
+export default App;
