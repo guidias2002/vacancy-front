70ec5ea82d0ba6c1773d570404423810dc115827
Guilherme Dias <guidias200220@gmail.com>
2024-11-07 09:53:35 -0300

    profilePageCandidate created

---
+++ src/pages/candidate/ProfilePageCandidate.tsx
@@ -0,0 +1,10 @@
+import SidebarCandidate from "../../components/SidebarCandidate";
+
+const ProfilePageCandidate = () => {
+
+    return (
+        <SidebarCandidate/>
+    )
+}
+
+export default ProfilePageCandidate;
