f25be647c9146e7ec392ad823a0db076d124f557
Guilherme Dias <guidias200220@gmail.com>
2024-11-12 11:27:18 -0300

    feat: curriculum component created

---
+++ src/components/Curriculum.tsx
@@ -0,0 +1,14 @@
+import AboutMeForm from "./aboutCandidate/AboutMe";
+
+import '../styles/Curriculum.css';
+
+const Curriculum = () => {
+
+    return (
+        <div className="main-curriculum">
+            <AboutMeForm/>
+        </div>
+    )
+}
+
+export default Curriculum;
