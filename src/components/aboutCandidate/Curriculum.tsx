d6d6a55035b71c43c4023f9aef313d98cc2c6f5a
Guilherme Dias <guidias200220@gmail.com>
2024-11-14 12:45:20 -0300

    feat: subTitle update

--- src/components/aboutCandidate/Curriculum.tsx
+++ src/components/aboutCandidate/Curriculum.tsx
@@ -1,12 +1,17 @@
 import AboutMeForm from "../../components/aboutCandidate/AboutMeForm";
 
 import '../../styles/Curriculum.css';
+import Title from "../Title";
 import ProfessionalExperienceForm from "./ProfessionalExperienceForm";
 
 const Curriculum = () => {
 
     return (
         <div className="main-curriculum">
+            <Title
+                title="Currículo"
+                subTitle="Visão Geral das Qualificações e Experiências Profissionais"
+            />
             <AboutMeForm/>
             <ProfessionalExperienceForm/>
         </div>
