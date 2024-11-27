f2bb7ad17b0f1d73032b5ae8cbd3497fa80921fc
Guilherme Dias <guidias200220@gmail.com>
2024-11-26 21:28:40 -0300

    feat: AcademicExperience type created

---
+++ src/types/AcademicExperience.ts
@@ -0,0 +1,11 @@
+export default interface AcademicExperience {
+    id: number;
+    course: string;
+    institution: string;
+    level: string;
+    status: string;
+    monthStart: string;
+    yearStart: number;
+    monthEnd: string;
+    yearEnd: number;
+}
