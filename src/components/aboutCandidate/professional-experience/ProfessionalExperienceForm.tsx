60a0d497d2b203bdda1b788c6b8c8d027065690f
Guilherme Dias <guidias200220@gmail.com>
2024-11-28 12:10:16 -0300

    feat: form popup added

--- src/components/aboutCandidate/professional-experience/ProfessionalExperienceForm.tsx
+++ src/components/aboutCandidate/professional-experience/ProfessionalExperienceForm.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Dialog, DialogContent, DialogTitle, Typography } from '@mui/material';
 
 import FormAccordion from '../../FormAccordion';
 import AddIcon from '@mui/icons-material/Add';
+import ProfessionalExperienceList from './ProfessionalExperienceList';
+import ProfessionalExperienceFormPopup from './ProfessionalExperienceFormPopup';
 
 const ProfessionalExperienceForm: React.FC = () => {
 
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <FormAccordion title='Experiência profissional'>
-
+            <ProfessionalExperienceList />
             <Box
                 sx={{
                     display: 'flex',
@@ -18,11 +25,18 @@ const ProfessionalExperienceForm: React.FC = () => {
                     padding: "20px 16px",
                     cursor: 'pointer'
                 }}
-
+                onClick={handleOpen}
             >
                 <Typography sx={{ fontWeight: 'bold', color: '#87aa68' }}>Adicionar Experiência</Typography>
                 <AddIcon sx={{ color: '#87aa68' }} />
             </Box>
+
+            <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
+                <DialogTitle>Adicionar Experiência Profissional</DialogTitle>
+                <DialogContent>
+                    <ProfessionalExperienceFormPopup onClose={handleClose} />
+                </DialogContent>
+            </Dialog>
 
         </FormAccordion >
     );
