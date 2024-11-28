cdcd9f5b4513694962062a494781753a2916390b
Guilherme Dias <guidias200220@gmail.com>
2024-11-27 21:03:38 -0300

    feat: ProfessionalExperience interface created

---
+++ src/types/ProfessionalExperience.ts
@@ -0,0 +1,11 @@
+export interface ProfessionalExperience {
+    id: number;
+    enterprise: string;
+    position: string;
+    monthStart: string;
+    yearStart: number;
+    monthEnd: string;
+    yearEnd: number;
+    isCurrentJob: boolean;
+    description: string;
+}
