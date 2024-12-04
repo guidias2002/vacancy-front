aab7de74168732f6ee6ad1d3a54b9738c535a205
Guilherme Dias <guidias200220@gmail.com>
2024-12-04 18:26:05 -0300

    feat: css updated

--- src/components/aboutCandidate/language-experience/LanguageForm.tsx
+++ src/components/aboutCandidate/language-experience/LanguageForm.tsx
@@ -1,12 +1,14 @@
-import { Autocomplete, Box, Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { Autocomplete, Box, Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useState } from 'react'
 import Language from '../../../types/Language';
+import AddIcon from '@mui/icons-material/Add';
 
 
 const LanguageForm: React.FC = () => {
 
     const candidateId = localStorage.getItem("candidateId");
+    const [createLanguage, setCreateLanguage] = useState(false);
     const [language, setLanguage] = useState<Language>({
         id: 0,
         language: "",
@@ -69,38 +71,60 @@ const LanguageForm: React.FC = () => {
     }
 
     return (
-        <Box
-            sx={{
-                display: "flex",
-                gap: 2,
-                width: '100%',
-                margin: "0 auto",
-            }}
-        >
-            <FormControl fullWidth>
-                <Autocomplete
-                    options={languages}
-                    value={language.language}
-                    onChange={(event, newValue) => handleChange("language", newValue || "")}
-                    renderInput={(params) => <TextField {...params} label="Idioma" />}
-                    fullWidth
-                />
-            </FormControl>
 
-            <FormControl fullWidth>
-                <Autocomplete
-                    options={levels}
-                    value={language.level}
-                    onChange={(event, newValue) => handleChange("level", newValue || "")}
-                    renderInput={(params) => <TextField {...params} label="Nível" />}
-                    fullWidth
-                />
-            </FormControl>
+        <>
+            {createLanguage ?
+                <Box
+                    sx={{
+                        display: "flex",
+                        gap: 2,
+                        width: '100%',
+                        margin: "0 auto",
+                    }}
+                >
+                    <FormControl fullWidth>
+                        <Autocomplete
+                            options={languages}
+                            value={language.language}
+                            onChange={(event, newValue) => handleChange("language", newValue || "")}
+                            renderInput={(params) => <TextField {...params} label="Idioma" />}
+                            fullWidth
+                        />
+                    </FormControl>
 
-            <Button variant="contained" onClick={handleSubmit} sx={{ bgcolor: "#87aa68", color: "white", width: '30%' }}>
-                Salvar
-            </Button>
-        </Box>
+                    <FormControl fullWidth>
+                        <Autocomplete
+                            options={levels}
+                            value={language.level}
+                            onChange={(event, newValue) => handleChange("level", newValue || "")}
+                            renderInput={(params) => <TextField {...params} label="Nível" />}
+                            fullWidth
+                        />
+                    </FormControl>
+
+                    <Button variant="contained" onClick={handleSubmit} sx={{ bgcolor: "#87aa68", color: "white", width: '30%' }}>
+                        Salvar
+                    </Button>
+                </Box>
+                :
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        border: "1px solid #87aa68",
+                        borderRadius: "4px",
+                        padding: "20px 16px",
+                        cursor: 'pointer'
+                    }}
+                    onClick={() => setCreateLanguage(true)}
+                >
+                    <Typography sx={{ fontWeight: 'bold', color: '#87aa68' }}>Adicionar Idioma</Typography>
+                    <AddIcon sx={{ color: '#87aa68' }} />
+                </Box>}
+
+
+
+        </>
     );
 }
 
