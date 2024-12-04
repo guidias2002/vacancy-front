866a827107cb0c114a351e9440ef0c71c7750355
Guilherme Dias <guidias200220@gmail.com>
2024-12-04 12:22:39 -0300

    feat: css updated

--- src/components/FormAccordion.tsx
+++ src/components/FormAccordion.tsx
@@ -32,15 +32,15 @@ const FormAccordion: React.FC<FormAccordionProps> = ({ title, children }) => {
             sx={{
                 width: '100%',
                 margin: '0 auto',
-                padding: '20px 5px',
+                padding: '10px 5px',
                 boxShadow: 'rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px',
-                borderRadius: '20px',
+                borderRadius: '10px',
                 '&.MuiAccordion-root:before': {
                     display: 'none',
                 },
                 '&:last-of-type': {
-                    borderBottomLeftRadius: '20px',
-                    borderBottomRightRadius: '20px',
+                    borderBottomLeftRadius: '10px',
+                    borderBottomRightRadius: '10px',
                 },
             }}
         >
