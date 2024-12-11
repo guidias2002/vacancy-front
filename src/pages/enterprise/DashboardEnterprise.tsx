1b46b535ab16b7970535adc30b7183950e80ef50
Guilherme Dias <guidias200220@gmail.com>
2024-12-11 11:27:59 -0300

    feat: DashboardEnterprise created

---
+++ src/pages/enterprise/DashboardEnterprise.tsx
@@ -0,0 +1,11 @@
+import SidebarCandidate from '../../components/Sidebar'
+
+const DashboardEnterprise = () => {
+  return (
+    <>
+      <SidebarCandidate/>
+    </>
+  )
+}
+
+export default DashboardEnterprise;
