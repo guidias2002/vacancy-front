e9989f069b6a10c60c922a9e60218c29de3c582c
Guilherme Dias <guidias200220@gmail.com>
2024-12-16 19:45:42 -0300

    feat: LoginRecruiterPage created

---
+++ src/pages/recruiter/LoginRecruiterPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RecruiterLoginForm from '../../components/recruiter/login/RecruiterLoginForm'
+
+const LoginRecruiterPage = () => {
+  return (
+    <RecruiterLoginForm/>
+  )
+}
+
+export default LoginRecruiterPage
