0597eb916951d69999df0075f98f2028eebe332d
Guilherme Dias <guidias200220@gmail.com>
2024-12-17 13:01:50 -0300

    feat: RecruiterData created

---
+++ src/types/RecruiterData.ts
@@ -0,0 +1,8 @@
+export default interface RecruiterData {
+    id: number,
+    name: string,
+    email: string,
+    enterpriseId: number,
+    accountType: string,
+    invitationStatus: string
+}
