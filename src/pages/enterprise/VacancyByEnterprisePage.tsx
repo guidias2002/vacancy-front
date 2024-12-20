afa32bb75152808b046148b931a5e475573d9527
Guilherme Dias <guidias200220@gmail.com>
2024-12-20 12:20:57 -0300

    feat: VacancyByEnterprisePage created

---
+++ src/pages/enterprise/VacancyByEnterprisePage.tsx
@@ -0,0 +1,30 @@
+import { Box, Divider } from '@mui/material'
+import React from 'react'
+import Sidebar from '../../components/Sidebar'
+import ListVacancyByEnterprise from '../../components/enterprise/vacancy/ListVacancyByEnterprise'
+import Title from '../../components/Title'
+
+
+const VacancyByEnterprisePage: React.FC = () => {
+    return (
+        <Box sx={{
+            display: 'flex',
+            height: '100vh',
+            bgcolor: '#fff',
+            width: '100%',
+            padding: '40px',
+            
+        }}>
+
+            <Sidebar />
+
+            <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', gap: 4 }}>
+                <Title title='Minhas Vagas' subTitle='Todas as vagas da empresa estão aqui'/>
+                <Divider/>
+                <ListVacancyByEnterprise/>
+            </Box>
+        </Box >
+    )
+}
+
+export default VacancyByEnterprisePage
