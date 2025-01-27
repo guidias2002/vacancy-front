12f2874ff786b9cc64cc3e73b6e47db50a52a0c4
Guilherme Dias <guidias200220@gmail.com>
2025-01-27 12:14:06 -0300

    feat: interface EnterpriseData created

---
+++ src/types/EnterpriseData.ts
@@ -0,0 +1,15 @@
+export interface EnterpriseData {
+    id: number;
+    name: string;
+    cnpj: string;
+    login: string;
+    email: string;
+    password: string;
+    accountType: "ENTERPRISE";
+    createdAt: string;
+    about: string;
+    sector: string;
+    linkedin: string;
+    webSite: string;
+    enterpriseId: number;
+}
